94cb3cc0a7852f06d0f43b796a48d94182578576
kawane041 <112392922+kawane041@users.noreply.github.com>
2024-08-06 22:11:26 -0300

    06/08/2024

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
@@ -6,71 +7,72 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Você está aprendendo sobre o papel das empresas de energia no Brasil e descobre que a Copel é uma das principais fornecedoras de energia do estado do Paraná. Qual o primeiro pensamento que lhe vem à mente?",
+        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
         alternativas: [
             {
-                texto: "É interessante ver como empresas locais têm um grande impacto na economia.",
-                afirmacao: "afirmação"
+                texto: "Isso é assustador!",
+                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
             },
             {
-                texto: "A dependência de uma única empresa para energia pode ser preocupante.",
-                afirmacao: "afirmação"
+                texto: "Isso é maravilhoso!",
+                afirmacao: "Quis saber como usar IA no seu dia a dia."
             }
         ]
     },
     {
-        enunciado: "Você está realizando uma pesquisa sobre como a Copel investe em energia renovável. A sua tarefa é escrever um relatório sobre isso. Qual abordagem você escolheria?",
+        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
         alternativas: [
             {
-                texto: "Utiliza informações disponíveis no site da Copel e em relatórios de sustentabilidade da empresa.",
-                afirmacao: "afirmação"
+                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
             },
             {
-                texto: "Baseia o relatório em entrevistas com pessoas que trabalham na empresa e em artigos acadêmicos sobre energia renovável.",
-                afirmacao: "afirmação"
+                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
             }
         ]
     },
     {
-        enunciado: "Durante uma palestra sobre o futuro da energia no Paraná, o palestrante menciona que a Copel está investindo em tecnologias de smart grid. Como você reage a essa informação?",
+        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
         alternativas: [
             {
-                texto: "Fico entusiasmado com as inovações tecnológicas que podem melhorar a eficiência da rede elétrica.",
-                afirmacao: "afirmação"
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
             },
             {
-                texto: "Preocupo-me com a implementação e os custos associados a essas novas tecnologias.",
-                afirmacao: "afirmação"
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
             }
         ]
     },
     {
-        enunciado: "Você foi desafiado a criar uma apresentação sobre o impacto das iniciativas da Copel no desenvolvimento sustentável da região. Qual ferramenta você usaria para criar essa apresentação?",
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
         alternativas: [
             {
-                texto: "Usaria um software de apresentação como o PowerPoint para criar gráficos e slides detalhados.",
-                afirmacao: "afirmação"
+                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
             },
             {
-                texto: "Utilizaria uma ferramenta de design gráfico para criar imagens e infográficos visualmente atraentes.",
-                afirmacao: "afirmação"
+                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
             }
         ]
     },
     {
-        enunciado: "Seu grupo está desenvolvendo um projeto de pesquisa sobre a eficiência energética e decidiu usar alguns dados fornecidos pela Copel. Ao revisar o projeto, você percebe que as informações estão muito semelhantes aos dados oficiais da empresa. O que você faz?",
+        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
         alternativas: [
             {
-                texto: "Utiliza os dados fornecidos sem alterações, pois são informações oficiais e confiáveis.",
-                afirmacao: "afirmação"
+                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
+                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
             },
             {
-                texto: "Analisa os dados, compara com outras fontes e adiciona insights próprios para garantir a originalidade do projeto.",
-                afirmacao: "afirmação"
+                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
+                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
             }
         ]
     },
 ];
+
 
 let atual = 0;
 let perguntaAtual;
@@ -104,9 +106,9 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPrincipal.style.display = "none";
-    caixaResultado.style.display = "block";
-    textoResultado.textContent = `Sua jornada: ${historiaFinal.trim()}`;
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
 }
 
 mostraPergunta();
